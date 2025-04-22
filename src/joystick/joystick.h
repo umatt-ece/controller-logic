8a8ac6690072b4479f650e8ffb9cce5b282e5876
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-04-21 23:41:52 -0500

    chore: structure initial joysticks source layout

---
+++ src/joystick/joystick.h
@@ -0,0 +1,23 @@
+/*******************************************************************************/
+/*!
+    \file joystick.h
+    \brief Header file for the joystick module.
+
+    Project     Controller Logic\n
+    Copyright   UMATT 2025\n
+    License     Use only under the terms of the contract / confidential\n
+
+    Date (YYYY/MM/DD) |   Author       |   Changes
+    ------------------|----------------|----------------
+    2025/04/21      |   Bruce Niemi  | File created
+ */
+/*******************************************************************************/
+
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+typedef struct JOYSTICK {
+  int dummy;  // Placeholder for joystick data
+} joystick_t;
+
+#endif  // JOYSTICK_H
