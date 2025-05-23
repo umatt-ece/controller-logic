37e630a055e8b87b70e2f8cf5524433c5eb993dc
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-05-23 13:28:37 -0500

    docs: update the docs

--- test/intergration_test.cpp
+++ test/intergration_test.cpp
@@ -50,9 +50,10 @@ void print_motor_status(const motor_t *motor) {
          direction_str);
 }
 
-void read_joystick_right(joystick_t *joystick) {
-  joystick->previous_position = joystick->current_position;
-  joystick->current_position = 230;
+void read_joystick_right(joystick_t *joystick) { // This function simulates the joystick see joystick.h for the bounds and ranges that you have to use for the "inputs"
+  joystick->previous_position = joystick->current_position; // This is the previous position of the joystick
+  joystick->current_position = 230; // You must actually do the bounding into the range of 0 to 256 (look at joystick.h)
+  // This should also be reading if the deadman switch is pressed
 }
 
 void read_joystick_left(joystick_t *joystick) {
@@ -60,19 +61,24 @@ void read_joystick_left(joystick_t *joystick) {
   joystick->current_position = 50;
 }
 
-void set_right_motor_speed(motor_t *motor) { print_motor_status(motor); }
+void set_right_motor_speed(motor_t *motor) { print_motor_status(motor); } // This will be called to set the motor todo the speed or what ever it needs to do
 
 void set_left_motor_speed(motor_t *motor) { print_motor_status(motor); }
 
-void seat_switch_pressed(bool *seat_pressed) { *seat_pressed = true; }
+void seat_switch_pressed(bool *seat_pressed) { *seat_pressed = true; } // This is a function that will be called to check if the seat is pressed
 
 TEST(ControllerLogicTest, Initialization) {
+  // This provides the main configuration for the controller logic to handle the action
   controller_logic_settings_t settings = {
       .dead_zone = 7,    // Area were we don't care about the joystick position
       .sensitivity = 5,  // How sensitive the joystick is
       .speed = MOTOR_SPEED_MEDIUM,  // Speed of the motor
-      .seat_pressed = seat_switch_pressed,
-      .read_right_joystick = read_joystick_right,
+      .seat_pressed = seat_switch_pressed, // this is a function pointer that will 
+                                          // be called to check if the seat is
+                                          // pressed
+      .read_right_joystick = read_joystick_right, // this is a function pointer
+                                          // that will be called to read the
+                                          // right joystick
       .read_left_joystick = read_joystick_left,
       .right_motor_set_speed = set_right_motor_speed,
       .left_motor_set_speed = set_left_motor_speed,
